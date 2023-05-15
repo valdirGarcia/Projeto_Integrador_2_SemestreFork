03336dc36a775ca4cf7fb1ae256255e779b93a71
pedr0tavi0 <112214113+pedr0tavi0@users.noreply.github.com>
2023-05-15 18:37:41 -0300

    base do site

---
+++ Site/pi_2semestre/script.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById('navLinks');
+
+function apareceMenu() {
+    navLinks.style.right = "0";
+}
+
+function escondeMenu() {
+    navLinks.style.right = "-200px";
+}
