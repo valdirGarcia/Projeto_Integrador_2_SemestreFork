dd1a5f419f89d10e30a48a5b2bed68b6a97cc137
pedr0tavi0 <112214113+pedr0tavi0@users.noreply.github.com>
2023-06-04 21:51:39 -0300

    Delete Site/pi_2semestre directory

--- Site/pi_2semestre/script.js
+++
@@ -1,9 +0,0 @@
-var navLinks = document.getElementById('navLinks');
-
-function apareceMenu() {
-    navLinks.style.right = "0";
-}
-
-function escondeMenu() {
-    navLinks.style.right = "-200px";
-}
